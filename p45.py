980c081f29d8674c29c56f7510a66eb15bd47e3e
Anisha <anishalathwalal1983@gmail.com>
2022-12-08 23:55:53 +0530

    Question...

---
+++ p45.py
@@ -0,0 +1,12 @@
+def div(a,b):
+    print(a/b)
+    
+def new_div(func):
+    def innerFunc(a,b):
+        if a<b:
+            a,b=b,a
+            return func(a,b)
+        return innerFunc
+        div= new_div(div)
+div(3,4)
+
