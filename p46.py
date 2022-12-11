75875ede74dfcf87d7bc33cd0e6e581c771dc404
Anisha <anishalathwalal1983@gmail.com>
2022-12-11 21:34:10 +0530

    Lambda function..

---
+++ p46.py
@@ -0,0 +1,11 @@
+# Triple all th elements in the list
+list1=[10,20,30,40,50]
+output= list(map(lambda i: i*3,list1))
+#print(output)
+
+
+list2=[34,88,30,22,10,15,18]
+output= list(filter(lambda i : i%5==0,list2))
+print(output)
+
+
